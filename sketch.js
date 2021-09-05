5d58551ed4947289f30336b51478ecd6c89544c0
Beatriz Sanchez <bia.lsanchez@gmail.com>
2021-09-05 10:19:33 -0300

    confirmar

--- sketch.js
+++ sketch.js
@@ -1,12 +1,11 @@
 var tela;
 var bloco1,bloco2,bloco3,bloco4;
 var bola, bordas;
-var music;
+var musica;
 
 function preload(){
-    music = loadSound("music.mp3");
+    // carregue o som aqui
 }
-
 
 function setup(){
     tela = createCanvas(800,600);
@@ -17,16 +16,11 @@ function setup(){
     bloco2 = createSprite(295,580,200,30);
     bloco2.shapeColor = "orange";
 
-    bloco3 = createSprite(515,580,200,30);
-    bloco3.shapeColor = "red";
-
-    bloco4 = createSprite(740,580,220,30);
-    bloco4.shapeColor = "green";
+    //crie aqui mais dois blocos, ou seja, bloco3 e bloco4
 
     bola = createSprite(random(20,750),100, 40,40);
     bola.shapeColor = rgb(255,255,255);
-    bola.velocityX = 4;
-    bola.velocityY = 9;
+    //escreva o código para adicionar velocityX e velocityY
 
 }
 
@@ -35,25 +29,24 @@ function draw() {
     bordas=createEdgeSprites();
     bola.bounceOff(bordas);
 
+    
+    //escreva o código para a bola quicar no bloco1
     if(bloco1.isTouching(bola) && bola.bounceOff(bloco1)){
         bola.shapeColor = "blue";
         music.play();
     }
 
+
     if(bloco2.isTouching(bola)){
         bola.shapeColor = "orange";
-        bola.velocityX = 0;
-        bola.velocityY = 0;
-        music.stop();
+        //escreva o código para definir velocityX e velocityY da bola como 0
+
+        //escreva o código para parar a música
     }
 
-    if(bloco3.isTouching(bola) && bola.bounceOff(bloco3)){
-        bola.shapeColor = "red";
-    }
+    //escreva o código para a bola quicar no bloco3
 
-    if(bloco4.isTouching(bola) && bola.bounceOff(bloco4)){
-        bola.shapeColor = "green";
-    }
+    //escreva o código para a bola quicar no bloco4
 
     drawSprites();
 }
